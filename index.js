bca6e9e09fbdc35d80eb5cfd0dd91976a0b9952e
Manmohandeep Singh <ltsbcsgo@gmail.com>
2022-12-17 21:09:16 -0500

    commit tree df5932a203f1b8ae3c623862cfe0bf601a539a6f

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let multiply;
+const num1 = 62;
+const num2 = 1;
+multiply = num1 * num2;
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod;
+let num3;
+let num4;
+num3 = 14;
+num4 = 5;
+mod = num3 % num4;
+
+
+let max;
+max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
